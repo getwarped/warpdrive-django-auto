8cef822d2e618b9817bad0a36e57afb6d80a5b62
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-06-20 20:49:25 +1000

    Add Django settings for logging and static files.

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -119,3 +119,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
